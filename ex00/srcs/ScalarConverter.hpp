355fa35e0e578679e15c0626643db336a945832f
NEIL-smtg <sunghui2000@hotmail.com>
2023-08-23 03:49:14 +0800

    initial commit

---
+++ ex00/srcs/ScalarConverter.hpp
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ScalarConverter.hpp                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: suchua < suchua@student.42kl.edu.my>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/09 19:41:24 by suchua            #+#    #+#             */
+/*   Updated: 2023/07/09 19:52:41 by suchua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SCALAR_CONVERTER_HPP
+# define SCALAR_CONVERTER_HPP
+
+# include <string>
+
+class ScalarConverter
+{
+	public:
+		static void	convert(std::string& val);
+};
+
+#endif
